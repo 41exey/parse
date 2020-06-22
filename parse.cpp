da8bda8999b908ff69d97fa876775cee392d0f1b
c1ewd <c1ewd@protonmail.com>
2020-06-23 03:13:00 +0700

    Was added parsing target number

--- parse.cpp
+++ parse.cpp
@@ -11,15 +11,15 @@ void help() {
 }
 
 void list() {
-	cout << endl;
 	cout << " 1. 127.0.0.1 - Windows 64" << endl;
 	cout << " 2. 132.32.78.154 - Linux x32_64" << endl;
 	cout << " 3. all" << endl;
+	cout << endl;
 }
 
-void payload() {
+void payload(unsigned int num) {
+	cout << "For target " << num << " will be try load pay" << endl;
 	cout << endl;
-	cout << "This load pay" << endl;
 }
 
 void error() {
@@ -39,9 +39,19 @@ int main() {
 		else if (command == "list")
 			list();
 		else if ((int) command.find("payload") > -1)
-			if (regex_match(command, regex("^payload\\s\\d+$")))
-				payload();
-			else {
+			if (regex_match(command, regex("^payload\\s\\d+$"))) {
+				static std::regex e("\\d+");
+				std::sregex_token_iterator i(command.begin(), command.end(), e);
+				std::sregex_token_iterator end;
+
+				std::string result;
+				while (i != end)
+					result += i->str() + " ",
+					++i;
+
+				cout << result << endl;
+				payload(stoi(result));
+			} else {
 				cout << "Required parameter lost" << endl;
 				cout << endl;
 			}
