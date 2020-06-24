0fe13672225eac9d8dae02be5bdcb089c58af91b
c1ewd <c1ewd@protonmail.com>
2020-06-24 19:14:24 +0700

    Refactoring help and list command

--- parse.cpp
+++ parse.cpp
@@ -3,15 +3,18 @@
 #include <regex>
 #include <thread>
 
+#include <boost/asio.hpp>
+
+using boost::asio::ip::tcp;
 using namespace std;
 
-void help() {
+void command_help() {
 	cout << endl;
 	cout << "This will be enough today" << endl;
 	cout << endl;
 }
 
-void list() {
+void command_list() {
 	cout << " 1. 127.0.0.1 - Windows 64" << endl;
 	cout << " 2. 132.32.78.154 - Linux x32_64" << endl;
 	cout << " 3. all" << endl;
@@ -37,9 +40,9 @@ void dialog() {
 		cout << ">";
 		getline(cin, command);
 		if (command == "help")
-			help();
+			command_help();
 		else if (command == "list")
-			list();
+			command_list();
 		else if ((int) command.find("payload") > -1)
 			if (regex_match(command, regex("^payload\\s\\d+$"))) {
 				static std::regex e("\\d+");
